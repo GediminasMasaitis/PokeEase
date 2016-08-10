d3b06f0136e27a09a0e5545ef57a0655090e917d
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2016-08-10 22:54:05 +0300

    Added initial page

---
+++ src/script/script.js
@@ -0,0 +1,32 @@
+var NecroWSClient = (function () {
+    function NecroWSClient(config) {
+        var _this = this;
+        this.connect = function () {
+            _this.client = new WebSocket(_this.config.serverURL);
+            _this.client.onopen = _this.clientOnOpen;
+            _this.client.onmessage = _this.clientOnMessage;
+        };
+        this.clientOnOpen = function () {
+            console.log("Connected to " + _this.client.url);
+        };
+        this.clientOnMessage = function (event) {
+            var message = JSON.parse(event.data);
+            console.log(message);
+            var type = message.$type;
+            if (_.includes(type, "UpdatePositionEvent")) {
+                var positionEvent = message;
+                if (_this.config.onLocationUpdate) {
+                    _this.config.onLocationUpdate(positionEvent);
+                }
+            }
+        };
+        this.config = config;
+    }
+    return NecroWSClient;
+}());
+$(function () {
+    var necroClient = new NecroWSClient({
+        serverURL: "ws://127.0.0.1:14252"
+    });
+    necroClient.connect();
+});
---
+++ src/script/script.ts
@@ -0,0 +1,52 @@
+﻿interface ILocation {
+    Latitude: number;
+    Longitude: number;
+}
+
+interface INecroWSClientParams {
+    serverURL: string;
+
+    onLocationUpdate?: (location: ILocation) => void;
+}
+
+class NecroWSClient {
+    private config: INecroWSClientParams;
+    private client : WebSocket;
+
+    constructor(config: INecroWSClientParams) {
+        this.config = config;
+    }
+
+    public connect = (): void => {
+        this.client = new WebSocket(this.config.serverURL);
+        this.client.onopen = this.clientOnOpen;
+        this.client.onmessage = this.clientOnMessage;
+    }
+
+    private clientOnOpen = (): void => {
+        console.log(`Connected to ${this.client.url}`);
+    }
+
+    private clientOnMessage = (event: MessageEvent): void => {
+        const message = JSON.parse(event.data);
+        console.log(message);
+
+        const type = message.$type as string;
+
+        if (_.includes(type, "UpdatePositionEvent")) {
+            const positionEvent = message as ILocation;
+            if (this.config.onLocationUpdate) {
+                this.config.onLocationUpdate(positionEvent);
+            }
+        }
+    }
+}
+
+$(() => {
+    var necroClient = new NecroWSClient({
+        serverURL: "ws://127.0.0.1:14252"
+    });
+
+    necroClient.connect();
+});
+
