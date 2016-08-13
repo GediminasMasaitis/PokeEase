dd9675782fea0aa0e57d4a86aa22caa66f728396
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2016-08-13 13:34:43 +0300

    Cleaned up notifications a bit

--- src/script/ts/Notifications/NotificationManager.ts
+++ src/script/ts/Notifications/NotificationManager.ts
@@ -38,172 +38,121 @@
         _.each(fortUsed.ItemsList, item => {
            const itemId = InventoryInfo.itemIds[item.Name];
            const itemName = this.config.translationManager.translation.itemNames[itemId];
-			
            itemsHtml += `<div class="item" title="${itemName}"><img src="images/items/${itemId}.png"/>x${item.Count}</div>`;
         });
 
-        const html = `<div class="event pokestop">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="info">
-                            ${itemsHtml}
-                            <div class="stats">+${fortUsed.Exp}XP</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["pokestop"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
+        const html = `<div class="info">
+                          ${itemsHtml}
+                          <div class="stats">+${fortUsed.Exp}XP</div>
+                      </div>`;
 
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: fortUsed
-        });
+        this.addNotification(fortUsed, html, "pokestop");
     }
 
     public addNotificationPokemonCapture = (pokemonCatch: IPokemonCaptureEvent): void => {
         const pokemonName = this.config.translationManager.translation.pokemonNames[pokemonCatch.Id];
         const roundedPerfection = Math.round(pokemonCatch.Perfection * 100) / 100;
-        const eventTyp = pokemonCatch.IsSnipe ? "snipe" : "catch";
-        const eventTypName = pokemonCatch.IsSnipe ? 
-		   this.config.translationManager.translation.eventTypes["snipe"]:
-		   this.config.translationManager.translation.eventTypes["catch"];
+        const eventType = pokemonCatch.IsSnipe ? "snipe" : "catch";
 
-        const html = `<div class="event ${eventTyp}">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="image">
+        const html = `<div class="image">
                             <img src="images/pokemon/${pokemonCatch.Id}.png"/>
                         </div>
                         <div class="info">
                             ${pokemonName}
                             <div class="stats">CP ${pokemonCatch.Cp} | IV ${roundedPerfection}%</div>
-                        </div>
-                        <span class="event-type">${eventTypName}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
+                        </div>`;
 
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: pokemonCatch
-        });
+        this.addNotification(pokemonCatch, html, eventType);
     }
 
     public addNotificationPokemonEvolved = (pokemonEvolve: IPokemonEvolveEvent): void => {
         const pokemonName = this.config.translationManager.translation.pokemonNames[pokemonEvolve.Id];
-        const html = `<div class="event evolve">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="image">
-                            <img src="images/pokemon/${pokemonEvolve.Id}.png"/>
-                        </div>
-                        <div class="info">
-                            ${pokemonName}
-                            <div class="stats">+${pokemonEvolve.Exp}XP</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["evolve"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
 
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: pokemonEvolve
-        });
+        const html = `<div class="image">
+                          <img src="images/pokemon/${pokemonEvolve.Id}.png"/>
+                      </div>
+                      <div class="info">
+                          ${pokemonName}
+                          <div class="stats">+${pokemonEvolve.Exp}XP</div>
+                      </div>`;
+
+        this.addNotification(pokemonEvolve, html, "evolve");
+    }
+
+    public addNotificationEggHatched = (eggHatched: IEggHatchedEvent): void => {
+        const pokemonName = this.config.translationManager.translation.pokemonNames[eggHatched.PokemonId];
+        const roundedPerfection = Math.round(eggHatched.Perfection * 100) / 100;
+
+        const html = `<div class="image">
+                          <img src="images/pokemon/${eggHatched.PokemonId}.png"/>
+                      </div>
+                      <div class="info">
+                          ${pokemonName}
+                          <div class="stats">CP ${eggHatched.Cp} | IV ${roundedPerfection}%</div>
+                      </div>`;
+
+        this.addNotification(eggHatched, html, "egg-hatched");
+    }
+
+    public addNotificationIncubatorStatus = (incubatorStatus: IIncubatorStatusEvent): void => {
+        const km = Math.round((incubatorStatus.KmToWalk - incubatorStatus.KmRemaining) * 100) / 100;
+
+        const html = `<div class="image">
+                          <img src="images/items/ItemEgg.png"/>
+                      </div>
+                      <div class="info">Egg
+                          <div class="stats">${km} of ${incubatorStatus.KmToWalk}km</div>
+                      </div>`;
+
+        this.addNotification(incubatorStatus, html, "incubator-status");
     }
 
     public addNotificationItemRecycle = (itemRecycle: IItemRecycleEvent): void => {
         const itemName = this.config.translationManager.translation.itemNames[itemRecycle.Id];
-        const html = `<div class="event recycle">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="info" title="${itemName}">
-                            <div class="item"><img src="images/items/${itemRecycle.Id}.png"/>x${itemRecycle.Count}</div>
-                            <div class="stats">+${itemRecycle.Count} free space</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["recycle"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
 
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: itemRecycle
-        });
+        const html = `<div class="info" title="${itemName}">
+                          <div class="item"><img src="images/items/${itemRecycle.Id}.png"/>x${itemRecycle.Count}</div>
+                          <div class="stats">+${itemRecycle.Count} free space</div>
+                      </div>`;
+
+        this.addNotification(itemRecycle, html, "recycle");
     }
 
     public addNotificationPokemonTransfer = (pokemonTransfer: IPokemonTransferEvent): void => {
         const pokemonName = this.config.translationManager.translation.pokemonNames[pokemonTransfer.Id];
         const roundedPerfection = Math.round(pokemonTransfer.Perfection * 100) / 100;
-        const html = `<div class="event transfer">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="image">
-                            <img src="images/pokemon/${pokemonTransfer.Id}.png"/>
+
+        const html = `<div class="image">
+                          <img src="images/pokemon/${pokemonTransfer.Id}.png"/>
+                      </div>
+                      <div class="info">
+                          ${pokemonName}
+                          <div class="stats">CP ${pokemonTransfer.Cp} | IV ${roundedPerfection}%</div>
+                      </div>`;
+
+       this.addNotification(pokemonTransfer, html, "transfer");
+    }
+
+    private addNotification = (event: IEvent, innerHtml: string, eventType: string): void => {
+        const eventTypeName = this.config.translationManager.translation.eventTypes[eventType];
+
+        const html=`<div class="event ${eventType}">
+                        <div class="item-container">
+                            <i class="fa fa-times dismiss"></i>
+                            ${innerHtml}
+                            <span class="event-type">${eventTypeName}</span>
+                            <span class="timestamp">0 seconds ago</span>
+                            <div class="category"></div>
                         </div>
-                        <div class="info">
-                            ${pokemonName}
-                            <div class="stats">CP ${pokemonTransfer.Cp} | IV ${roundedPerfection}%</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["transfer"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
                     </div>`;
 
         const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: pokemonTransfer
+        element.click(this.closeNotification);
+        this.config.container.append(element);
+        this.notifications.push({
+            event: event,
+            element: element
         });
-    }
-	
-    public addNotificationEggHatched = (eggHatched: IEggHatchedEvent): void => {
-        const html = `<div class="event eggHatched">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="image">
-                            <img src="images/pokemon/${eggHatched.PokemonId}.png"/>
-                        </div>
-                        <div class="info">Egg
-                            <div class="stats">CP ${eggHatched.Cp} | IV ${eggHatched.Perfection}% | LvL ${eggHatched.Level}</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["eggHatched"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
-
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: eggHatched
-        });
-    }
-    public addNotificationIncubatorStatus = (incubatorStatus: IIncubatorStatusEvent): void => {
-
-		var km =  Math.round((incubatorStatus.KmToWalk - incubatorStatus.KmRemaining)*100)/100;
-        const html = `<div class="event incubatorStatus">
-                        <i class="fa fa-times dismiss"></i>
-                        <div class="image">
-                            <img src="images/items/ItemEgg.png"/>
-                        </div>
-                        <div class="info">Egg
-                            <div class="stats">${km} of ${incubatorStatus.KmToWalk}km</div>
-                        </div>
-                        <span class="event-type">${this.config.translationManager.translation.eventTypes["incubatorStatus"]}</span>
-                        <span class="timestamp">0 seconds ago</span>
-                        <div class="category"></div>
-                    </div>`;
-
-        const element = $(html);
-        this.addNotificationFinal({
-            element: element,
-            event: incubatorStatus
-        });
-    }
-
-    private addNotificationFinal = (notification: INotification): void => {
-        notification.element.wrapInner('<div class="item-container"></div>');
-        notification.element.click(this.closeNotification);
-        this.config.container.append(notification.element);
-        this.notifications.push(notification);
         this.config.container.animate({
             scrollTop: this.config.container.prop("scrollHeight") - this.config.container.height()
         }, 100);
