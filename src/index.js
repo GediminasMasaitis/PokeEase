2dec7b5363719164a33ef8ab4478b33a0b2512ea
Alessandro Pezzè <igougi.ui@gmail.com>
2016-08-25 09:24:51 +0200

    Heroku support node

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+    fs = require('fs');
+
+try {
+	fs.readFile('./index.html', function (err, html) {
+	    if (err) {
+	        throw err; 
+	    }       
+	    http.createServer(function(request, response) {  
+	        response.writeHeader(200, {"Content-Type": "text/html"});  
+	        response.write(html);  
+	        response.end();  
+	    }).listen(process.env.PORT || 8080);
+	});
+} catch (e) {
+	console.log(e)
+}
